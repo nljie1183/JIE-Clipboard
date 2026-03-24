a80e3be1d1c6f5a4a3bc10f75655f2953b4bfe26
nljie1183 <3170076995@qq.com>
2026-03-25 02:46:11 +0800

    调整限制大小

--- Pages/AboutPage.cs
+++ Pages/AboutPage.cs
@@ -40,7 +40,7 @@ public class AboutPage : UserControl
 
         var versionLabel = new Label
         {
-            Text = "版本 3.0.0.4",
+            Text = "版本 3.0.0.5",
             Font = new Font(ThemeService.GlobalFont.FontFamily, 11f),
             ForeColor = ThemeService.SecondaryTextColor,
             AutoSize = true,
